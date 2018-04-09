f54d8deaafa30a5b4b20231bf825df9cfef86e62
JuliaYa <julia.yakutova@gmail.com>
2018-04-09 21:15:17 +0400

    355. Refactoring and more tests.

--- 355_cipher.js
+++ 355_cipher.js
@@ -1,14 +1,15 @@
-function cipher(secret, message){
-  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-  const alphabetLength = alphabet.length;
-  const table = {};
-
-
+function Cipher(){
+  this.alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  this.table = {};
+  const alphabetLength = this.alphabet.length;
+  
   for(var i = 0; i < alphabetLength; i++){
-    var letter = alphabet[i];
-    table[letter] = alphabet.substr(i) + alphabet.substr(0, i);
+    var letter = this.alphabet[i];
+    this.table[letter] = this.alphabet.substr(i) + this.alphabet.substr(0, i);
   }
+};
 
+Cipher.prototype.encrypt = function(secret, message){
   var messageLength = message.length;
   var fullRepeats = Math.floor(messageLength / secret.length);
   var rest = messageLength % secret.length;
@@ -18,10 +19,14 @@ function cipher(secret, message){
   for(var i = 0; i < messageLength; i++){
     var letterSecret = secretKey[i];
     var letterMessage = message[i];
-    var secretInd = alphabet.indexOf(letterSecret);
-    cipheredMessage += table[letterMessage][secretInd];
+    var secretInd = this.alphabet.indexOf(letterSecret);
+    cipheredMessage += this.table[letterMessage][secretInd];
   }
   return cipheredMessage;
+};
+
+Cipher.prototype.decript = function(){
+
 }
 
-module.exports = cipher;
+module.exports = Cipher;
--- tests/355_cipher.test.js
+++ tests/355_cipher.test.js
@@ -1,5 +1,27 @@
-const cipher = require('../355_cipher');
+const Cipher = require('../355_cipher');
 
-test('cipher message right way', () => {
-  expect(cipher('snitch', 'thepackagehasbeendelivered')).toBe('lumicjcnoxjhkomxpkwyqogywq');
+test('message "thepackagehasbeendelivered" with secret "snitch" return "lumicjcnoxjhkomxpkwyqogywq"', () => {
+  var cipher = new Cipher();
+  var encriptedMessage = cipher.encrypt('snitch', 'thepackagehasbeendelivered');
+  expect(encriptedMessage).toBe('lumicjcnoxjhkomxpkwyqogywq');
+});
+
+// Challenge Inputs
+
+test('message "theredfoxtrotsquietlyatmidnight" with secret "bond" return "uvrufrsryherugdxjsgozogpjralhvg"', () => {
+  var cipher = new Cipher();
+  var encriptedMessage = cipher.encrypt('bond', 'theredfoxtrotsquietlyatmidnight');
+  expect(encriptedMessage).toBe('uvrufrsryherugdxjsgozogpjralhvg');
+});
+
+test('message "murderontheorientexpress" with secret "train" return "flrlrkfnbuxfrqrgkefckvsa"', () => {
+  var cipher = new Cipher();
+  var encriptedMessage = cipher.encrypt('train', 'murderontheorientexpress');
+  expect(encriptedMessage).toBe('flrlrkfnbuxfrqrgkefckvsa');
+});
+
+test('message "themolessnuckintothegardenlastnight" with secret "garden" return "zhvpsyksjqypqiewsgnexdvqkncdwgtixkx"', () => {
+  var cipher = new Cipher();
+  var encriptedMessage = cipher.encrypt('garden', 'themolessnuckintothegardenlastnight');
+  expect(encriptedMessage).toBe('zhvpsyksjqypqiewsgnexdvqkncdwgtixkx');
 });
